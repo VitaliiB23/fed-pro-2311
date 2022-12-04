75a56bbef6a6792a6b56c2a44a27cc790b92277b
vitaliib23 <2004vitalii2004@gmail.com>
2022-12-04 20:17:42 +0200

    HW3011

---
+++ 30112022/js/script.js
@@ -0,0 +1,27 @@
+// 1
+let firstValue = prompt("answer one ");
+let secondValue = prompt("answer two ");
+console.log(Number(firstValue) + Number(secondValue));
+console.log(Number(firstValue) / Number(secondValue));
+
+// 2
+let FirstVar = prompt("first Name");
+let SecondVar = prompt("Last Name");
+document.write('User Name: ', FirstVar);
+document.write('<br>');
+document.write('User last name: ', SecondVar);
+
+// 3
+let uk = prompt(1);
+let ru = prompt(1);
+let trust = prompt(true);
+let goodPeople = prompt(false);
+let rain = uk == trust;
+let rain1 = ru == goodPeople;
+console.log(rain);
+console.log(rain1);
+
+// 4
+let a = 22;
+let b = 32;
+console.log('task4', a > 20 && b > 30);
