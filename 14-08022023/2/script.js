84f339bfd9d0e3b000321ec330b59a743947e6a7
VitaliiB23 <2004vitalii2004@gmail.com>
2023-02-22 18:54:41 +0200

    HW14-2

--- 14-08022023/2/script.js
+++ 14-08022023/2/script.js
@@ -1,58 +1,56 @@
-const planetsList = document.getElementById("planets-list");
-let planets = [];
+window.onload = () => {
+  const list = document.querySelector("div.list-group");
+  const img = document.querySelector("section img");
 
-function getPlanets() {
-  const url = "https://swapi.dev/api/planets";
-  fetch(url)
-    .then(response => response.json())
-    .then(data => {
-      planets = data.results;
-      renderPlanets(planets);
-    });
+  async function getAllDogs() {
+      const response = await fetch("https://dog.ceo/api/breeds/list/all");
+      const responseJson = await response.json();
+      return responseJson.message;
+  }
+
+  getAllDogs().then(dogs => {
+      function addDog(breed, subBreed = "") {
+          const input = document.createElement("input");
+          input.setAttribute("type", "radio");
+          input.className = "btn-check";
+          input.setAttribute("name", "vbtn-radio");
+          input.setAttribute("id", breed + subBreed);
+          input.setAttribute("autocomplete", "off");
+
+          input.onchange = function (e) {
+              if (e.target.checked) {
+                  const endpoint = subBreed === "" ? breed : `${breed}/${subBreed}`;
+                  fetch(`https://dog.ceo/api/breed/${endpoint}/images/random`).then(
+                      resp => resp.json()
+                  ).then(response => {
+                      img.setAttribute("src", response.message);
+                      img.setAttribute("alt", endpoint)
+                  })
+              }
+          }
+
+          list.appendChild(input);
+
+          const label = document.createElement("label");
+          label.className = "btn btn-outline-primary rounded-0 border-secondary list-group-item-action";
+          label.style.borderTopWidth = "0";
+          label.style.borderBottomWidth = "1px";
+          label.style.borderInlineWidth = "0";
+          label.htmlFor = breed + subBreed;
+          label.innerText = subBreed === "" ?
+              `${breed.charAt(0).toUpperCase()}${breed.slice(1)}` :
+              `${breed.charAt(0).toUpperCase()}${breed.slice(1)} ${subBreed.charAt(0).toUpperCase()}${subBreed.slice(1)}`;
+          list.appendChild(label);
+      }
+
+      for (const breed in dogs) {
+          if (dogs[breed].length === 0) {
+              addDog(breed)
+          } else if (dogs[breed].length > 0) {
+              for (const subBreed of dogs[breed]) {
+                  addDog(breed, subBreed);
+              }
+          }
+      }
+  })
 }
-
-function renderPlanets(planets) {
-  planetsList.innerHTML = "";
-  planets.forEach(planet => {
-    const card = document.createElement("div");
-    card.classList.add("planet-card");
-
-    const name = document.createElement("div");
-    name.classList.add("planet-name");
-    name.textContent = planet.name;
-
-    const diameter = document.createElement("div");
-    diameter.classList.add("planet-detail");
-    diameter.textContent = `Diameter: ${planet.diameter}`;
-
-    const population = document.createElement("div");
-    population.classList.add("planet-detail");
-    population.textContent = `Population: ${planet.population}`;
-
-    card.appendChild(name);
-    card.appendChild(diameter);
-    card.appendChild(population);
-
-    planetsList.appendChild(card);
-  });
-}
-
-function sortByDiameter() {
-  planets.sort((a, b) => {
-    const aDiameter = parseInt(a.diameter) || 0;
-    const bDiameter = parseInt(b.diameter) || 0;
-    return aDiameter - bDiameter;
-  });
-  renderPlanets(planets);
-}
-
-function sortByPopulation() {
-  planets.sort((a, b) => {
-    const aPopulation = parseInt(a.population) || 0;
-    const bPopulation = parseInt(b.population) || 0;
-    return aPopulation - bPopulation;
-  });
-  renderPlanets(planets);
-}
-
-getPlanets();
